95b4d127aca57840132dac3d38ddb980e2e6b0f9
Jony <jamsi123@bk.ru>
2020-07-22 01:14:52 +0300

    launcher modify

--- launcher.py
+++ launcher.py
@@ -1,17 +1,21 @@
 import subprocess
 from platform import system
+import sys
 
 
-def main():
+def main(count=2):
     if system() == 'Linux':
         subprocess.call(['gnome-terminal', '-e', 'python server.py'])
-        subprocess.call(['gnome-terminal', '-e', 'python client.py'])
-        subprocess.call(['gnome-terminal', '-e', 'python client.py'])
+        for _ in range(count):
+            subprocess.call(['gnome-terminal', '-e', 'python client.py'])
     elif system() == 'Windows':
-        subprocess.call('python python server.py', creationflags=subprocess.CREATE_NEW_CONSOLE)
-        subprocess.call('python python client.py', creationflags=subprocess.CREATE_NEW_CONSOLE)
-        subprocess.call('python python client.py', creationflags=subprocess.CREATE_NEW_CONSOLE)
-
+        subprocess.call('python server.py', creationflags=subprocess.CREATE_NEW_CONSOLE)
+        for _ in range(count):
+            subprocess.call('python client.py', creationflags=subprocess.CREATE_NEW_CONSOLE)
+        
 
 if __name__ == '__main__':
-    main()
+    if sys.argv[1].isdigit() and int(sys.argv[1])<10:
+        main(int(sys.argv[1]))
+    else:
+        main()
